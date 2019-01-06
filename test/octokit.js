1193b27754d67a4c1b30c173a1bb7ce745db5cfe
Simon Grondin <github@simongrondin.name>
2019-01-06 12:18:02 -0600

    fix(tests): Increased the error margin on an edge case test

--- test/octokit.js
+++ test/octokit.js
@@ -4,13 +4,13 @@ const throttlingPlugin = require('..')
 
 module.exports = Octokit
   .plugin((octokit) => {
-    const t0 = Date.now()
+    octokit.__t0 = Date.now()
     octokit.__requestLog = []
     octokit.__requestTimings = []
 
     octokit.hook.wrap('request', async (request, options) => {
       octokit.__requestLog.push(`START ${options.method} ${options.url}`)
-      octokit.__requestTimings.push(Date.now() - t0)
+      octokit.__requestTimings.push(Date.now() - octokit.__t0)
       await new Promise(resolve => setTimeout(resolve, 0))
 
       const res = options.request.responses.shift()
@@ -20,7 +20,7 @@ module.exports = Octokit
         throw error
       } else {
         octokit.__requestLog.push(`END ${options.method} ${options.url}`)
-        octokit.__requestTimings.push(Date.now() - t0)
+        octokit.__requestTimings.push(Date.now() - octokit.__t0)
         return res
       }
     })
