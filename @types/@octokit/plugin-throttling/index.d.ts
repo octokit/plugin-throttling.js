442ca1f0c05f5086c3087ba236801458a6fb268d
Doug Ayers <4142577+douglascayers@users.noreply.github.com>
2019-09-22 16:04:50 -0500

    Removed inner 'declare'
    Resolves "Error: A 'declare' modifier cannot be used in an already ambient context" with typescript v3.5.3

--- @types/@octokit/plugin-throttling/index.d.ts
+++ @types/@octokit/plugin-throttling/index.d.ts
@@ -1,5 +1,5 @@
 declare module '@octokit/plugin-throttling' {
   import Octokit from '@octokit/rest'
-  declare function plugin(octokit: Octokit, options: Octokit.Options): void
+  function plugin(octokit: Octokit, options: Octokit.Options): void
   export = plugin
 }
