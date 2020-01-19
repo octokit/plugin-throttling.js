a5eb84bf0e8e2cab1e1b93848f19c2f64d284ca7
Gregor Martynus <gregor@martynus.net>
2020-01-18 21:49:23 -0800

    style: prettier

--- scripts/update-endpoints/code.js
+++ scripts/update-endpoints/code.js
@@ -4,19 +4,19 @@
  * trigger notifications. So instead we automatically generate a file that
  * only contains these paths when @octokit/routes has a new release.
  */
-const { writeFileSync } = require('fs')
+const { writeFileSync } = require("fs");
 
-const ENDPOINTS = require('./generated/endpoints.json')
-const paths = []
+const ENDPOINTS = require("./generated/endpoints.json");
+const paths = [];
 
 for (const endpoint of ENDPOINTS) {
   if (endpoint.triggersNotification) {
-    paths.push(endpoint.url.replace(/\{([^}]+)}/g, ':$1'))
+    paths.push(endpoint.url.replace(/\{([^}]+)}/g, ":$1"));
   }
 }
 
-const uniquePaths = [...new Set(paths.sort())]
+const uniquePaths = [...new Set(paths.sort())];
 writeFileSync(
-  './lib/triggers-notification-paths.json',
-  JSON.stringify(uniquePaths, null, 2) + '\n'
-)
+  "./lib/triggers-notification-paths.json",
+  JSON.stringify(uniquePaths, null, 2) + "\n"
+);
