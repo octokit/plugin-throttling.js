69f747bd57a22c0463d3a0860da05a355630f466
Oscar Dominguez <dominguez.celada@gmail.com>
2022-03-10 15:38:20 +0100

    refactor(types): apply second iteration on trying to improve types for ThrottlingOptions

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,49 @@
 import { Octokit } from "@octokit/core";
+import Bottleneck from "bottleneck";
 
 declare module "@octokit/core/dist-types/types.d" {
   interface OctokitOptions {
-    throttle: ThrottlingOptions;
+    throttle?: ThrottlingOptions;
   }
 }
 
 type LimitHandler = (
-  retryAfter: number,
-  options: ThrottlingOptions,
-  octokit: Octokit
+  retryAfter?: number,
+  options?: object,
+  octokit?: Octokit
 ) => void;
 
-interface ThrottlingOptions {
+export type AbuseLimitHandler = {
+  /**
+   * @deprecated "[@octokit/plugin-throttling] `onAbuseLimit()` is deprecated and will be removed in a future release of `@octokit/plugin-throttling`, please use the `onSecondaryRateLimit` handler instead"
+   */
+  onAbuseLimit: LimitHandler;
+};
+
+export type SecondaryLimitHandler = {
+  onSecondaryRateLimit: LimitHandler;
+};
+
+export type ThrottlingOptionsBase = {
   enabled?: boolean;
   Bottleneck?: any;
   id?: string;
   timeout?: number;
   connection?: any;
-  /**
-   * @deprecated The method should not be used
-   */
-  onAbuseLimit: LimitHandler;
-  onSecondaryRateLimit: LimitHandler;
+  minimumSecondaryRateRetryAfter?: number;
+  retryAfterBaseValue?: number;
+  write?: Bottleneck.Group;
+  search?: Bottleneck.Group;
+  notifications?: Bottleneck.Group;
   onRateLimit: LimitHandler;
-}
+};
+
+export type ThrottlingOptions = ThrottlingOptionsBase &
+  (AbuseLimitHandler | SecondaryLimitHandler);
+
+export type Groups = {
+  global?: Bottleneck.Group;
+  write?: Bottleneck.Group;
+  search?: Bottleneck.Group;
+  notifications?: Bottleneck.Group;
+};
