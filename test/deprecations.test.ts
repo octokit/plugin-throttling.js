f095e0b572897c6e3cfb87624935b4c3196ae1f5
wolfy1339 <webmaster@wolfy1339.com>
2023-05-21 19:00:30 -0400

    fix: remove deprecated methods

--- test/deprecations.test.ts
+++ test/deprecations.test.ts
@@ -3,44 +3,4 @@ import { throttling } from "../src";
 
 const TestOctokit = Octokit.plugin(throttling);
 
-describe("deprecations", () => {
-  it("throttle.minimalSecondaryRateRetryAfter option", () => {
-    const log = {
-      warn: jest.fn(),
-    };
-    new TestOctokit({
-      // @ts-expect-error
-      log,
-      throttle: {
-        minimalSecondaryRateRetryAfter: 1,
-        onSecondaryRateLimit: () => 1,
-        onRateLimit: () => 1,
-      },
-    });
-
-    expect(log.warn).toHaveBeenCalledWith(
-      "[@octokit/plugin-throttling] `options.throttle.minimalSecondaryRateRetryAfter` is deprecated, please use `options.throttle.fallbackSecondaryRateRetryAfter` instead"
-    );
-  });
-
-  describe("throttle.onAbuseLimit", function () {
-    it("Should detect SecondaryRate limit and broadcast event", async function () {
-      const log = {
-        warn: jest.fn(),
-      };
-
-      new TestOctokit({
-        // @ts-expect-error
-        log,
-        throttle: {
-          onAbuseLimit: () => 1,
-          onRateLimit: () => 1,
-        },
-      });
-
-      expect(log.warn).toHaveBeenCalledWith(
-        "[@octokit/plugin-throttling] `onAbuseLimit()` is deprecated and will be removed in a future release of `@octokit/plugin-throttling`, please use the `onSecondaryRateLimit` handler instead"
-      );
-    });
-  });
-});
+describe.skip("deprecations", () => {});
