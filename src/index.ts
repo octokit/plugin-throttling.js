46e933f7fe62aaeb4f86040d5c4030bf5134e8a5
Jeffrey Yasskin <jyasskin@google.com>
2023-10-23 20:46:58 -0700

    GraphQL queries can exceed the rate limit even with x-ratelimit-remaining !== 0.

--- src/index.ts
+++ src/index.ts
@@ -137,8 +137,9 @@ export function throttling(octokit: Octokit, octokitOptions: OctokitOptions) {
         return { wantRetry, retryAfter };
       }
       if (
-        error.response.headers != null &&
-        error.response.headers["x-ratelimit-remaining"] === "0"
+        (error.response.headers != null &&
+         error.response.headers["x-ratelimit-remaining"] === "0"
+        ) || (error.response.data?.errors ?? []).some((error) => error.type === "RATE_LIMITED")
       ) {
         // The user has used all their allowed calls for the current time period (REST and GraphQL)
         // https://docs.github.com/en/rest/reference/rate-limit (REST)
